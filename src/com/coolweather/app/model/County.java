29e3ad72a1e5e1bf8e8a5f7f8f51a27c71099794
Langi <281148327@qq.com>
2015-10-12 12:20:03 +0800

    新增数据库帮助类，以及各表对应的实体类。

---
+++ src/com/coolweather/app/model/County.java
@@ -0,0 +1,37 @@
+package com.coolweather.app.model;
+
+public class County {
+
+	private int id;
+	private String CountyName;
+	private String CountyCode;
+	private int CityId;
+	
+	public int getId(){
+		return id;
+	}
+	public void setId(int id){
+		this.id = id;
+	}
+	
+	public String getCountyName(){
+		return CountyName;
+	}
+	public void setCountyName(String countyName){
+		this.CountyName = countyName;
+	}
+	
+	public String getCountyCode(){
+		return CountyCode;
+	}
+	public void setCountyCode(String countyCode){
+		this.CountyCode = countyCode;
+	}
+	
+	public int getCityId(){
+		return CityId;
+	}
+	public void setCityId(int cityId){
+		this.CityId = cityId;
+	}
+}
