88dcc9ac46e3a3943144639967779d2ba9ecbc70
Langi <281148327@qq.com>
2015-10-16 16:19:01 +0800

    增加后台自动更新天气功能

--- src/com/coolweather/app/activity/WeatherActivity.java
+++ src/com/coolweather/app/activity/WeatherActivity.java
@@ -3,6 +3,7 @@ package com.coolweather.app.activity;
 
 import com.coolweather.app.R;
 import com.coolweather.app.model.County;
+import com.coolweather.app.service.AutoUpdateService;
 import com.coolweather.app.util.HttpCallbackListener;
 import com.coolweather.app.util.HttpUtil;
 import com.coolweather.app.util.Utility;
@@ -194,6 +195,8 @@ public class WeatherActivity extends Activity {
 		currentDateText.setText(prefs.getString("current_date", ""));
 		weatherInfoLayout.setVisibility(View.VISIBLE);
 		cityNameText.setVisibility(View.VISIBLE);
+		Intent intent = new Intent(this, AutoUpdateService.class);
+		startService(intent);
 	}
 	
 	@Override
